a746b4a98fa244eb460074c2287a5117d524c172
MatiasGuichard <matyguichard@gmail.com>
2024-03-23 20:27:33 -0300

    ejercicios terminados

--- ejercicios/04.js
+++ ejercicios/04.js
@@ -4,6 +4,12 @@ function sumarArray(arrayOfNumbers, cb) {
   // Este resultado debes pasárselo como argumento al callback recibido.
   // NOTA: no debes retOrnar nada.
   // Tu código:
+
+  var suma = arrayOfNumbers.reduce(function(acum, elem) {
+    return acum + elem;
+  }, 0);
+
+  cb(suma);
 }
 
 module.exports = sumarArray;
--- ejercicios/07.js
+++ ejercicios/07.js
@@ -2,6 +2,13 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
+
+  var newArray = arrayOfStrings.filter(function(element) {
+  return element[0] === 'a' || element[0] === 'A';
+  });
+
+  return newArray;
 }
+
 
 module.exports = filter;
